d4ca00d5f2d3dffce5e34abc0d30950f4f815dc9
kevinmatula <Matulakevin91@gmail.com>
2025-11-21 19:22:49 -0500

    changed around home page; got rid of terminal

---
+++ kevinmatula.com/src/app/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar.tsx";
+import "./App.css";
+import Home from "./home/page.tsx";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ kevinmatula.com/src/app/home/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="buttons-home">
+      <div>
+        <a href="/">About</a>
+      </div>
+      <div>
+        <a href="/">Projects</a>
+      </div>
+      <div>
+        <a href="/">Notebook</a>
+      </div>
+    </div>
+  );
+}
